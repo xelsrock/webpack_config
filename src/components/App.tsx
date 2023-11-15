8c6305c3fcf189ebfb849781e56b821474f77992
xelsrock <alex.perevosnicov@gmail.com>
2023-11-15 20:10:03 +0300

    webpack config

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './App.scss';
+
+export const App = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => {
+    setCount(prev => prev + 1);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>increment</button>
+    </div>
+  )
+}
